62618911c069733ca17aac87663b3f9c90515d30
Chethan-Aradhya-16 <aradhyachethan457@gmail.com>
2026-08-13 15:11:26 +0530

    add delete task feature

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ button.addEventListener("click", function () {
         const li = document.createElement("li");
         li.textContent = task;
 
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+
+        deleteButton.addEventListener("click", function () {
+            li.remove();
+        });
+
+        li.appendChild(deleteButton);
         taskList.appendChild(li);
 
         input.value = "";
