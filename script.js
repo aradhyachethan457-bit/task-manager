93343123e4f75705da2049f84864fa3a94de2f59
Chethan-Aradhya-16 <aradhyachethan457@gmail.com>
2026-08-18 18:37:22 +0530

    add task priority feature (#3)

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 console.log("Urgent bug fix");
 
 const input = document.querySelector("input");
-const button = document.querySelector("button");
+const button = document.querySelector("#addTask");
 const taskList = document.querySelector("#taskList");
+const priority = document.querySelector("#priority");
 
 button.addEventListener("click", function () {
     const task = input.value;
+    const taskPriority = priority.value;
 
     if (task !== "") {
         const li = document.createElement("li");
-        li.textContent = task;
+        li.textContent = `${task} - Priority: ${taskPriority}`;
 
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete";
